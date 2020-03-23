0c4b6d0dc16ddf8c8f549ba496be9dc027c43234
xXRedbridgeXx <marcusrodbro@gmail.com>
2020-03-23 14:26:49 +0100

    oprydning

--- Eksamensprojekt/Eksamensprojekt/Form1.vb
+++ Eksamensprojekt/Eksamensprojekt/Form1.vb
@@ -1,13 +1,2 @@
 ﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'Mega lol
-    End Sub
-
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
-
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        End
-    End Sub
 End Class
