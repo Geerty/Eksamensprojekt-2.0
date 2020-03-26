47761dc098fcefb4d04e3674df6285536d682a5f
Kasper Geert <kasp553c@elev.tradium.dk>
2020-03-26 13:48:24 +0100

    26-03-2020

--- Eksamensprojekt/Eksamensprojekt/Form1.Designer.vb
+++ Eksamensprojekt/Eksamensprojekt/Form1.Designer.vb
@@ -36,25 +36,29 @@ Partial Class Form1
         Me.Lbl_Sc = New System.Windows.Forms.Label()
         Me.Lbl_Sb = New System.Windows.Forms.Label()
         Me.Lbl_Sa = New System.Windows.Forms.Label()
+        Me.But_Nulstil = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.But_Beregn = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Tbox_Va
         '
-        Me.Tbox_Va.Location = New System.Drawing.Point(57, 124)
+        Me.Tbox_Va.Location = New System.Drawing.Point(138, 87)
         Me.Tbox_Va.Name = "Tbox_Va"
         Me.Tbox_Va.Size = New System.Drawing.Size(100, 20)
         Me.Tbox_Va.TabIndex = 0
         '
         'Tbox_Vb
         '
-        Me.Tbox_Vb.Location = New System.Drawing.Point(57, 161)
+        Me.Tbox_Vb.Location = New System.Drawing.Point(138, 124)
         Me.Tbox_Vb.Name = "Tbox_Vb"
         Me.Tbox_Vb.Size = New System.Drawing.Size(100, 20)
         Me.Tbox_Vb.TabIndex = 1
         '
         'Tbox_Vc
         '
-        Me.Tbox_Vc.Location = New System.Drawing.Point(57, 197)
+        Me.Tbox_Vc.Location = New System.Drawing.Point(138, 160)
         Me.Tbox_Vc.Name = "Tbox_Vc"
         Me.Tbox_Vc.Size = New System.Drawing.Size(100, 20)
         Me.Tbox_Vc.TabIndex = 2
@@ -62,7 +66,7 @@ Partial Class Form1
         'Lbl_vinkler
         '
         Me.Lbl_vinkler.AutoSize = True
-        Me.Lbl_vinkler.Location = New System.Drawing.Point(13, 86)
+        Me.Lbl_vinkler.Location = New System.Drawing.Point(94, 49)
         Me.Lbl_vinkler.Name = "Lbl_vinkler"
         Me.Lbl_vinkler.Size = New System.Drawing.Size(39, 13)
         Me.Lbl_vinkler.TabIndex = 6
@@ -71,7 +75,7 @@ Partial Class Form1
         'Lbl_sider
         '
         Me.Lbl_sider.AutoSize = True
-        Me.Lbl_sider.Location = New System.Drawing.Point(13, 241)
+        Me.Lbl_sider.Location = New System.Drawing.Point(94, 204)
         Me.Lbl_sider.Name = "Lbl_sider"
         Me.Lbl_sider.Size = New System.Drawing.Size(31, 13)
         Me.Lbl_sider.TabIndex = 10
@@ -79,21 +83,21 @@ Partial Class Form1
         '
         'Tbox_Sc
         '
-        Me.Tbox_Sc.Location = New System.Drawing.Point(57, 352)
+        Me.Tbox_Sc.Location = New System.Drawing.Point(138, 315)
         Me.Tbox_Sc.Name = "Tbox_Sc"
         Me.Tbox_Sc.Size = New System.Drawing.Size(100, 20)
         Me.Tbox_Sc.TabIndex = 9
         '
         'Tbox_Sb
         '
-        Me.Tbox_Sb.Location = New System.Drawing.Point(57, 316)
+        Me.Tbox_Sb.Location = New System.Drawing.Point(138, 279)
         Me.Tbox_Sb.Name = "Tbox_Sb"
         Me.Tbox_Sb.Size = New System.Drawing.Size(100, 20)
         Me.Tbox_Sb.TabIndex = 8
         '
         'Tbox_Sa
         '
-        Me.Tbox_Sa.Location = New System.Drawing.Point(57, 279)
+        Me.Tbox_Sa.Location = New System.Drawing.Point(138, 242)
         Me.Tbox_Sa.Name = "Tbox_Sa"
         Me.Tbox_Sa.Size = New System.Drawing.Size(100, 20)
         Me.Tbox_Sa.TabIndex = 7
@@ -101,7 +105,7 @@ Partial Class Form1
         'Lbl_Va
         '
         Me.Lbl_Va.AutoSize = True
-        Me.Lbl_Va.Location = New System.Drawing.Point(29, 127)
+        Me.Lbl_Va.Location = New System.Drawing.Point(110, 90)
         Me.Lbl_Va.Name = "Lbl_Va"
         Me.Lbl_Va.Size = New System.Drawing.Size(14, 13)
         Me.Lbl_Va.TabIndex = 11
@@ -110,7 +114,7 @@ Partial Class Form1
         'Lbl_Vb
         '
         Me.Lbl_Vb.AutoSize = True
-        Me.Lbl_Vb.Location = New System.Drawing.Point(29, 164)
+        Me.Lbl_Vb.Location = New System.Drawing.Point(110, 127)
         Me.Lbl_Vb.Name = "Lbl_Vb"
         Me.Lbl_Vb.Size = New System.Drawing.Size(14, 13)
         Me.Lbl_Vb.TabIndex = 12
@@ -119,7 +123,7 @@ Partial Class Form1
         'Lbl_Vc
         '
         Me.Lbl_Vc.AutoSize = True
-        Me.Lbl_Vc.Location = New System.Drawing.Point(29, 200)
+        Me.Lbl_Vc.Location = New System.Drawing.Point(110, 163)
         Me.Lbl_Vc.Name = "Lbl_Vc"
         Me.Lbl_Vc.Size = New System.Drawing.Size(14, 13)
         Me.Lbl_Vc.TabIndex = 13
@@ -128,7 +132,7 @@ Partial Class Form1
         'Lbl_Sc
         '
         Me.Lbl_Sc.AutoSize = True
-        Me.Lbl_Sc.Location = New System.Drawing.Point(29, 355)
+        Me.Lbl_Sc.Location = New System.Drawing.Point(110, 318)
         Me.Lbl_Sc.Name = "Lbl_Sc"
         Me.Lbl_Sc.Size = New System.Drawing.Size(13, 13)
         Me.Lbl_Sc.TabIndex = 16
@@ -137,7 +141,7 @@ Partial Class Form1
         'Lbl_Sb
         '
         Me.Lbl_Sb.AutoSize = True
-        Me.Lbl_Sb.Location = New System.Drawing.Point(29, 319)
+        Me.Lbl_Sb.Location = New System.Drawing.Point(110, 282)
         Me.Lbl_Sb.Name = "Lbl_Sb"
         Me.Lbl_Sb.Size = New System.Drawing.Size(13, 13)
         Me.Lbl_Sb.TabIndex = 15
@@ -146,17 +150,46 @@ Partial Class Form1
         'Lbl_Sa
         '
         Me.Lbl_Sa.AutoSize = True
-        Me.Lbl_Sa.Location = New System.Drawing.Point(29, 282)
+        Me.Lbl_Sa.Location = New System.Drawing.Point(110, 245)
         Me.Lbl_Sa.Name = "Lbl_Sa"
         Me.Lbl_Sa.Size = New System.Drawing.Size(13, 13)
         Me.Lbl_Sa.TabIndex = 14
         Me.Lbl_Sa.Text = "a"
+        '
+        'But_Nulstil
+        '
+        Me.But_Nulstil.Location = New System.Drawing.Point(394, 371)
+        Me.But_Nulstil.Name = "But_Nulstil"
+        Me.But_Nulstil.Size = New System.Drawing.Size(140, 50)
+        Me.But_Nulstil.TabIndex = 17
+        Me.But_Nulstil.Text = "Nulstil"
+        Me.But_Nulstil.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(387, 49)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(300, 300)
+        Me.PictureBox1.TabIndex = 18
+        Me.PictureBox1.TabStop = False
+        '
+        'But_Beregn
+        '
+        Me.But_Beregn.Location = New System.Drawing.Point(547, 371)
+        Me.But_Beregn.Name = "But_Beregn"
+        Me.But_Beregn.Size = New System.Drawing.Size(140, 50)
+        Me.But_Beregn.TabIndex = 19
+        Me.But_Beregn.Text = "Beregn"
+        Me.But_Beregn.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.But_Beregn)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.But_Nulstil)
         Me.Controls.Add(Me.Lbl_Sc)
         Me.Controls.Add(Me.Lbl_Sb)
         Me.Controls.Add(Me.Lbl_Sa)
@@ -173,6 +206,7 @@ Partial Class Form1
         Me.Controls.Add(Me.Tbox_Va)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -192,4 +226,7 @@ Partial Class Form1
     Friend WithEvents Lbl_Sc As Label
     Friend WithEvents Lbl_Sb As Label
     Friend WithEvents Lbl_Sa As Label
+    Friend WithEvents But_Nulstil As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents But_Beregn As Button
 End Class
