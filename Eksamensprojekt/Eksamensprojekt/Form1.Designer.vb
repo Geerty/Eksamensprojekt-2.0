14741b152274eff9551aadafb606d911accebebf
Kasper Geert <kasp553c@elev.tradium.dk>
2020-03-26 14:11:41 +0100

    Baggrundskift

--- Eksamensprojekt/Eksamensprojekt/Form1.Designer.vb
+++ Eksamensprojekt/Eksamensprojekt/Form1.Designer.vb
@@ -186,6 +186,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Highlight
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.But_Beregn)
         Me.Controls.Add(Me.PictureBox1)
